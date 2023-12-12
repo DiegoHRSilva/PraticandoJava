4a4368a29d2fbae7de2a059757ff8a7b3e93eb49
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2023-12-12 11:45:09 -0300

    GeradorPersonagem
    criado o programa com objetivo de gerar ficha de personagem de D&D automático

---
+++ GeradorPersonagem/Main.java
@@ -0,0 +1,53 @@
+package ded;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite o nome do personagem:");
+
+    String nome = scanner.nextLine();
+
+    Random random = new Random();
+
+    int numeroClasse = random.nextInt(6) + 1;
+
+    Personagem personagem = null;
+
+    switch (numeroClasse) {
+      case 1:
+        personagem = new Guerreiro();
+        break;
+      case 2:
+        personagem = new Ladino();
+        break;
+      case 3:
+        personagem = new Barbaro();
+        break;
+      case 4:
+        personagem = new Mago();
+        break;
+      case 5:
+        personagem = new Monge();
+        break;
+      case 6:
+        personagem = new Bardo();
+        break;
+    }
+
+    System.out.println("Nome:" + nome);
+
+    personagem.ordenarArrays();
+
+    personagem.imprimirValores();
+
+    personagem.geraTotal();
+
+  }
+
+}
