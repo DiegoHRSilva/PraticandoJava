da02fb7e6edb1d4b6d645f64892a4eef9673d2ca
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2023-12-13 11:27:22 -0300

    Update Bardo.java

--- GeradorPersonagem/Bardo.java
+++ GeradorPersonagem/Bardo.java
@@ -1,14 +1,26 @@
 package ded;
 
+import java.util.Arrays;
+
 public class Bardo extends Personagem {
 
-  public void geraTotal() {
-
-    somaCarisma = calcularAtributo(carisma) + 2;
+  @Override
+  public void imprimirValores() {
+    System.out
+        .println("Força: " + Arrays.toString(forca) + " -> " + calcularAtributo(forca));
+    System.out
+        .println("Destreza: " + Arrays.toString(destreza) + " -> " + calcularAtributo(destreza));
+    System.out.println(
+        "Constituição: " + Arrays.toString(constituicao) + " -> " + calcularAtributo(constituicao));
+    System.out.println(
+        "Inteligência: " + Arrays.toString(inteligencia) + " -> " + calcularAtributo(inteligencia));
+    System.out
+        .println("Sabedoria: " + Arrays.toString(sabedoria) + " -> " + calcularAtributo(sabedoria));
+    System.out
+        .println("Carisma: " + Arrays.toString(carisma) + " -> " + calcularAtributo(carisma));
 
     System.out.println(
-        "Minha classe é Bardo e meu atributo bonus é Carisma -> " + somaCarisma);
-
+        "Minha classe é Bardo e meu atributo bonus é Carisma -> "
+            + (calcularAtributo(carisma) + 2));
   }
-
 }
