8440de0f30dff7611ba313c8b1ec462c548b2340
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2023-12-13 10:11:04 -0300

    Update Barbaro.java

--- D&D2/Barbaro.java
+++ D&D2/Barbaro.java
@@ -1,14 +1,28 @@
 package ded2;
 
+import java.util.Arrays;
+
 public class Barbaro extends Personagem {
 
-  public void geraTotal() {
+  @Override
+  public void imprimirValores() {
 
-    somaConstituicao = calcularAtributo(constituicao) + 2;
+    System.out
+        .println("Força: " + Arrays.toString(forca) + " -> " + calcularAtributo(forca));
+    System.out
+        .println("Destreza: " + Arrays.toString(destreza) + " -> " + calcularAtributo(destreza));
+    System.out.println(
+        "Constituição: " + Arrays.toString(constituicao) + " -> " + calcularAtributo(constituicao));
+    System.out.println(
+        "Inteligência: " + Arrays.toString(inteligencia) + " -> " + calcularAtributo(inteligencia));
+    System.out
+        .println("Sabedoria: " + Arrays.toString(sabedoria) + " -> " + calcularAtributo(sabedoria));
+    System.out
+        .println("Carisma: " + Arrays.toString(carisma) + " -> " + calcularAtributo(carisma));
 
     System.out.println(
-        "Minha classe é Barbaro e meu atributo bonus é Constituicao -> " + somaConstituicao);
+        "Minha classe é Barbaro e meu atributo bonus é Constituicao -> "
+            + (calcularAtributo(constituicao) + 2));
 
   }
-
 }
