14d4270ad3fc513a569e3a348a62ea1487031332
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2023-12-13 10:12:38 -0300

    Update Monge.java

--- D&D2/Monge.java
+++ D&D2/Monge.java
@@ -1,14 +1,28 @@
 package ded2;
 
+import java.util.Arrays;
+
 public class Monge extends Personagem {
 
-  public void geraTotal() {
-
-    somaSabedoria = calcularAtributo(sabedoria) + 2;
-
+  @Override
+  public void imprimirValores() {
+    System.out
+        .println("Força: " + Arrays.toString(forca) + " -> " + calcularAtributo(forca));
+    System.out
+        .println(
+            "Destreza: " + Arrays.toString(destreza) + " -> " + calcularAtributo(destreza));
     System.out.println(
-        "Minha classe é Monge e meu atributo bonus é Sabedoria -> " + somaSabedoria);
+        "Constituição: " + Arrays.toString(constituicao) + " -> " + calcularAtributo(constituicao));
+    System.out.println(
+        "Inteligência: " + Arrays.toString(inteligencia) + " -> " + calcularAtributo(inteligencia));
+    System.out
+        .println(
+            "Sabedoria: " + Arrays.toString(sabedoria) + " -> " + calcularAtributo(sabedoria));
+    System.out
+        .println("Carisma: " + Arrays.toString(carisma) + " -> " + calcularAtributo(carisma));
+
+    System.out.println("Minha classe é Monge e meu atributo bonus é Sabedoria -> "
+        + (calcularAtributo(sabedoria) + 2));
 
   }
-
 }
