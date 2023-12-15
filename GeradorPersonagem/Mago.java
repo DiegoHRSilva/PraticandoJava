df68d5fe5eeec4f79406426d62b2a9e3c7e24640
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2023-12-15 11:20:50 -0300

    Update Mago.java

--- GeradorPersonagem/Mago.java
+++ GeradorPersonagem/Mago.java
@@ -4,23 +4,59 @@ import java.util.Arrays;
 
 public class Mago extends Personagem {
 
+  public Mago(String nome) {
+    super(nome);
+  }
+
+  @Override
+  public void geraPersonagem() {
+
+    setForca();
+    setDestreza();
+    setConstituicao();
+    setInteligencia();
+    setSabedoria();
+    setCarisma();
+
+  }
+
   @Override
   public void imprimirValores() {
-    System.out
-        .println("Força: " + Arrays.toString(forca) + " -> " + calcularAtributo(forca));
-    System.out
-        .println(
-            "Destreza: " + Arrays.toString(destreza) + " -> " + calcularAtributo(destreza));
+
+    System.out.println("------------------------------------------------------------------------");
+
+    System.out.println("Nome: " + getNome());
+
+    System.out.println("------------------------------------------------------------------------");
+
     System.out.println(
-        "Constituição: " + Arrays.toString(constituicao) + " -> " + calcularAtributo(constituicao));
+        "Força: " + Arrays.toString(getForca()) +
+            " -> " + calcularAtributo(getForca()));
+
     System.out.println(
-        "Inteligência: " + Arrays.toString(inteligencia) + " -> " + calcularAtributo(inteligencia));
-    System.out
-        .println("Sabedoria: " + Arrays.toString(sabedoria) + " -> " + calcularAtributo(sabedoria));
-    System.out
-        .println("Carisma: " + Arrays.toString(carisma) + " -> " + calcularAtributo(carisma));
+        "Destreza: " + Arrays.toString(getDestreza()) +
+            " -> " + calcularAtributo(getDestreza()));
+
+    System.out.println(
+        "Constituição: " + Arrays.toString(getConstituicao()) +
+            " -> " + calcularAtributo(getConstituicao()));
+
+    System.out.println(
+        "Inteligência: " + Arrays.toString(getInteligencia()) +
+            " -> " + calcularAtributo(getInteligencia()));
+
+    System.out.println(
+        "Sabedoria: " + Arrays.toString(getSabedoria()) +
+            " -> " + calcularAtributo(getSabedoria()));
+
+    System.out.println(
+        "Carisma: " + Arrays.toString(getCarisma()) +
+            " -> " + calcularAtributo(getCarisma()));
 
     System.out.println("Minha classe é Mago e meu atributo bonus é Inteligencia -> "
-        + (calcularAtributo(inteligencia) + 2));
+        + (calcularAtributo(getInteligencia()) + 2));
+
+    System.out.println("------------------------------------------------------------------------");
+
   }
 }
